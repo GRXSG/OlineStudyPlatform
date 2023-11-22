38a5aa2bdeed09b7c48ee464eca3c93e28935815
GRXSG <2277243795@qq.com>
2023-11-22 09:52:43 +0800

    登录注册功能

---
+++ src/main/java/top/wennyy/OlineStudyPlatformApplication.java
@@ -0,0 +1,13 @@
+package top.wennyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlineStudyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlineStudyPlatformApplication.class, args);
+    }
+
+}
